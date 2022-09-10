8cf2f912349c1c9bdaecd3a42f9c9d984ca8276f
Timo Peter <timo_peter@gmx.de>
2022-09-10 13:07:11 +0200

    move members from AppComponent to SplitViewService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,50 +20,50 @@ export class AppComponent {
   }
 
   ngOnInit() {
-    this.router.events
-      .pipe(takeUntil(this.destroyed$))
-      .subscribe(event => {
-        if (event instanceof NavigationEnd) {
-          if (event){
-            this.currentRoute = event.url ? event.url : '';
-            console.log(`currentRoute: ${this.currentRoute}`);
+    // this.router.events
+    //   .pipe(takeUntil(this.destroyed$))
+    //   .subscribe(event => {
+    //     if (event instanceof NavigationEnd) {
+    //       if (event){
+    //         this.currentRoute = event.url ? event.url : '';
+    //         console.log(`currentRoute: ${this.currentRoute}`);
 
-            // This logic should not reside here, maybe in SplitViewService.
-            // Just put here for simple example...
-            if (!this.SplitActive && this.RouteForSplitViewAreaBGiven && this.SplitViewOrderedForward){
-              this.changeOrder();
-            }
-            if (!this.SplitActive && this.RouteForSplitViewAreaAGiven && !this.SplitViewOrderedForward){
-              this.changeOrder();
-            }
-          }
-          // console.log('event:');
-          // console.log(event);
-        }
-      });
+    //         // This logic should not reside here, maybe in SplitViewService.
+    //         // Just put here for simple example...
+    //         if (!this.SplitActive && this.RouteForSplitViewAreaBGiven && this.SplitViewOrderedForward){
+    //           this.changeOrder();
+    //         }
+    //         if (!this.SplitActive && this.RouteForSplitViewAreaAGiven && !this.SplitViewOrderedForward){
+    //           this.changeOrder();
+    //         }
+    //       }
+    //       // console.log('event:');
+    //       // console.log(event);
+    //     }
+    //   });
   }
 
-  ngOnDestroy() {
-    this.destroyed$.next(null);
-  }
+  // ngOnDestroy() {
+  //   this.destroyed$.next(null);
+  // }
 
-  changeOrder() {
-    this.splitViewService.orderedForward = !this.splitViewService.orderedForward;
-  }
+  // changeOrder() {
+  //   this.splitViewService.orderedForward = !this.splitViewService.orderedForward;
+  // }
 
   get SplitViewOrderedForward() {
-    return this.splitViewService.orderedForward;
+    return this.splitViewService.OrderedForward;
   }
 
-  get RouteForSplitViewAreaAGiven() {
-    return this.currentRoute.includes('a:');
+  get SplitViewAreaAVisible() {
+    return this.splitViewService.AreaAVisible;
   }
 
-  get RouteForSplitViewAreaBGiven() {
-    return this.currentRoute.includes('b:');
+  get SplitViewAreaBVisible() {
+    return this.splitViewService.AreaBVisible;
   }
 
-  get SplitActive() {
-    return this.RouteForSplitViewAreaAGiven && this.RouteForSplitViewAreaBGiven;
+  get SplitViewSplitModeOn() {
+    return this.splitViewService.SplitModeOn;
   }
 }
--- src/app/split-view.service.ts
+++ src/app/split-view.service.ts
@@ -1,23 +1,67 @@
 import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map, Subject, takeUntil } from 'rxjs';
 import { SplitViewArea } from './split-view-area';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SplitViewService {
-  orderedForward = true;
+  private orderedForward = true;
 
-  areas: SplitViewArea[] = [
-    {
-        Name: 'A',
-        Index: 0
-    },
-    {
-        Name: 'B',
-        Index: 1
-    }
-  ];
+  private router: Router;
+  private destroyed$ = new Subject();
+  private currentRoute = '';
 
-  constructor() { }
+  constructor(router: Router) {
+    this.router = router;
+    this.init();
+  }
 
+  private init() {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(event => event as NavigationEnd),
+        takeUntil(this.destroyed$)
+      )
+      .subscribe(event => {
+        // Store current route
+        this.currentRoute = event.url ? event.url : '';
+
+        // If B is the only active element set reverse order (if not already done).
+        if (!this.SplitModeOn && this.AreaBVisible && this.orderedForward) {
+          this.changeOrder();
+        }
+
+        // If A is the only active element set forward order (if not already done).
+        if (!this.SplitModeOn && this.AreaAVisible && !this.orderedForward) {
+          this.changeOrder();
+        }
+      });
+  }
+
+  changeOrder() {
+    this.orderedForward = !this.orderedForward;
+  }
+
+  get OrderedForward() {
+    return this.orderedForward;
+  }
+
+  get AreaAVisible() {
+    return this.currentRoute.includes('a:');
+  }
+
+  get AreaBVisible() {
+    return this.currentRoute.includes('b:');
+  }
+
+  get SplitModeOn() {
+    return this.AreaAVisible && this.AreaBVisible;
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+  }
 }
